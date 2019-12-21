be6f387ca5af6857159e6cabf31fb76cc053b10f
Mario Galarza <mgalarza@learsoft.com.ar>
2019-12-20 22:23:46 -0300

    Refactoring de codigo

--- src/main/java/ar/com/marete/basic/entidades/Persona.java
+++ src/main/java/ar/com/marete/basic/entidades/Persona.java
@@ -12,27 +12,27 @@ public class Persona {
 	private String nombre;
 	private String apellidos;
 	private int edad;
-	
+
 	public String getNombre() {
 		return nombre;
 	}
-	
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	
+
 	public String getApellidos() {
 		return apellidos;
 	}
-	
+
 	public void setApellidos(String apellidos) {
 		this.apellidos = apellidos;
 	}
-	
+
 	public int getEdad() {
 		return edad;
 	}
-	
+
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
@@ -52,5 +52,5 @@ public class Persona {
 		super();
 		this.nombre = nombre;
 	}
-	
+
 }
